edcf8a0c9e029d274137bdcc1f03f3fde1942b28
NCrashed <ncrashed@localhost.localdomain>
2015-11-29 15:56:38 +0300

    GameModule instance for GLFWInputT

--- glfw-input/src/Game/GoreAndAsh/Input/GLFW/Module.hs
+++ glfw-input/src/Game/GoreAndAsh/Input/GLFW/Module.hs
@@ -10,11 +10,22 @@ import Control.Monad.Fix
 
 import Game.GoreAndAsh
 
-data GLFWState = GLFWState {
-  
+data GLFWState s = GLFWState {
+  glfwNextState :: s 
 } deriving (Generic)
 
-instance NFData GLFWState
+instance NFData s => NFData (GLFWState s)
 
-newtype GLFWInputT m a = GLFWInputT { runGLFWInputT :: StateT GLFWState m a }
-  deriving (Functor, Applicative, Monad, MonadState GLFWState, MonadFix)
+newtype GLFWInputT m s a = GLFWInputT { runGLFWInputT :: StateT (GLFWState s) m a }
+  deriving (Functor, Applicative, Monad, MonadState (GLFWState s), MonadFix)
+
+instance GameModule m s => GameModule (GLFWInputT m s) (GLFWState s) where 
+  runModule (GLFWInputT m) s = do
+    ((a, s'), nextState) <- runModule (runStateT m s) (glfwNextState s)
+    return (a, s' { glfwNextState = nextState })
+
+  newModuleState = do
+    s <- newModuleState 
+    return $ GLFWState {
+        glfwNextState = s
+      }
