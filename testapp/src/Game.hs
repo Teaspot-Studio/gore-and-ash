d9e737195ab19008283cec0740c2aa33224ef7bf
NCrashed <NCrashed@gmail.com>
2015-12-03 01:31:05 +0300

    Logging module debug, moved testapp out of library package

--- testapp/src/Game.hs
+++ testapp/src/Game.hs
@@ -5,12 +5,17 @@ module Game(
   , Game(..)
   ) where
 
+import Prelude hiding (id, (.))
 import Data.Functor.Identity
 import Game.GoreAndAsh
 import Control.Wire 
 import Control.DeepSeq
 import GHC.Generics (Generic)
 import Linear
+
+import Game.GoreAndAsh.Logging
+
+type AppWire a b = GameWire (LoggingT () Identity) a b
 
 data Player = Player {
   playerPos :: !(V2 Float)
@@ -35,7 +40,7 @@ data Game = Game {
 
 instance NFData Game 
 
-mainWire :: GameWire Identity a Game
+mainWire :: AppWire a Game
 mainWire = Game
   <$> playerWire initialPlayer
   <*> cameraWire initialCamera 
@@ -43,12 +48,13 @@ mainWire = Game
     initialCamera = Camera 0 0 0
     initialPlayer = Player 0 (V3 1 0 0) 0
 
-cameraWire :: Camera -> GameWire Identity a Camera 
+cameraWire :: Camera -> AppWire a Camera 
 cameraWire initialCamera = loop $ proc (_, c_) -> do 
   c <- delay initialCamera -< c_
   forceNF -< (c, c)
 
-playerWire :: Player -> GameWire Identity a Player 
+playerWire :: Player -> AppWire a Player 
 playerWire initialPlayer = loop $ proc (_, p_) -> do 
   p <- delay initialPlayer -< p_ 
+  traceEventShow . now -< "Hi, logging!"
   forceNF -< (p, p)
