23535b04e1b1cdf9ceb4661b9333759679229015
NCrashed <NCrashed@gmail.com>
2016-01-09 15:32:36 +0300

    Split module

---
+++ sync/src/Game/GoreAndAsh/Sync/Remote/Actor.hs
@@ -0,0 +1,42 @@
+module Game.GoreAndAsh.Sync.Remote.Actor(
+    RemoteActor(..)
+  , RemActorId(..)
+  , RemActorNetMessage(..)
+  ) where
+
+import Control.Wire
+import Data.Proxy 
+import Data.Word 
+import GHC.Generics
+import Prelude hiding ((.), id)
+
+import Game.GoreAndAsh.Actor
+import Game.GoreAndAsh.Sync.Message 
+
+-- | API to support automatic synchronization of actors between client and server
+class NetworkMessage i => RemoteActor i a | i -> a, a -> i where
+  type RemoteActorState i :: *
+  type RemoteActorId a :: *
+  -- | Unique id for synchronization actor attached to your actor
+  remoteActorFingerprint :: Proxy i -> Word64 
+  -- | Unique id for synchronization collection of remote actors
+  remoteActorCollectionFingerpring :: Proxy i -> Word64 
+
+-- | Id of synchronization actor build over another actor
+newtype RemActorId i = RemActorId { unRemActorId :: i }
+  deriving (Show, Eq, Ord, Generic)
+
+-- | Stub for local remote actor API
+data RemActorMessage i 
+
+instance RemoteActor i a => ActorMessage (RemActorId i) where
+  type ActorMessageType (RemActorId i) = RemActorMessage i
+  toCounter = toCounter . unRemActorId
+  fromCounter = RemActorId . fromCounter
+  actorFingerprint _ = remoteActorFingerprint (Proxy :: Proxy i)
+
+-- | Network protocol of synchronization actor
+data RemActorNetMessage i = RemActorNetMessageStub
+
+instance RemoteActor i a => NetworkMessage (RemActorId i) where
+  type NetworkMessageType (RemActorId i) = RemActorNetMessage i
---
+++ sync/src/Game/GoreAndAsh/Sync/Remote/Collection.hs
@@ -0,0 +1,93 @@
+module Game.GoreAndAsh.Sync.Remote.Collection(
+    RemActorCollId(..)
+  , remoteActorCollectionServer
+  , remoteActorCollectionClient
+  ) where
+
+import Control.DeepSeq
+import Control.Wire
+import Data.Serialize
+import GHC.Generics
+import Prelude hiding ((.), id)
+
+import Game.GoreAndAsh.Actor
+import Game.GoreAndAsh.Logging 
+import Game.GoreAndAsh.Network
+import Game.GoreAndAsh.Sync.Message 
+
+import Game.GoreAndAsh.Sync.Remote.Actor
+
+-- | Unique id space for remote collections actors
+newtype RemActorCollId = RemActorCollId { unRemActorCollId :: Int } 
+  deriving (Show, Eq, Ord, Generic)
+
+-- | Stub for local collection API
+data RemActorCollMessage
+
+-- | Fingerprint that is occupied is '4567348'
+instance ActorMessage RemActorCollId where
+  type ActorMessageType RemActorCollId = RemActorCollMessage
+  toCounter = unRemActorCollId
+  fromCounter = RemActorCollId
+  actorFingerprint _ = 4567348
+
+-- | Comminucation protocol between client and server side collections
+data RemActorCollNetMessage =
+  -- | Sent to client when server collection adds new actor
+    RemActorCollNetSpawn !Int 
+  -- | Sent to client when server collection removes specific actor
+  | RemActorCollNetDespawn !Int 
+  -- | Sent when remote collection is set up to get actors that were 
+  -- created before the collection creation
+  | RemActorCollRequestOthers 
+  deriving (Generic)
+
+instance NFData RemActorCollNetMessage
+instance Serialize RemActorCollNetMessage
+
+instance NetworkMessage RemActorCollId where
+  type NetworkMessageType RemActorCollId = RemActorCollNetMessage
+
+-- | Server side collection of network actors that are automatically
+-- synchronized to remote clients.
+-- 
+-- Second wire input is event with new actors to add to the collection
+-- Third wire input is event with id of actor to delete from the collection
+remoteActorCollectionServer :: (LoggingMonad m, ActorMonad m, NetworkMonad m, Eq i, RemoteActor i b) 
+  => [GameActor m i a b] -- ^ Initial set of actors
+  -> GameActor m RemActorCollId (a, Event [GameActor m i a b], Event [i]) [b]
+remoteActorCollectionServer initalActors = makeActor $ \_ -> proc (a, addEvent, remEvent) -> do 
+  dynCollection initalActors -< (a, addEvent, remEvent)
+
+-- | Internal state of client side collection of remote actors
+data ClientRemCollState i = ClientRemCollState {
+  clientRemCollNewActors :: [i]
+, clientRemCollDelActors :: [i]
+}
+
+emptyClientRemCollState :: ClientRemCollState i 
+emptyClientRemCollState = ClientRemCollState {
+    clientRemCollNewActors = []
+  , clientRemCollDelActors = []
+  }
+
+-- | Client side collection of network actors that are automatically
+-- synchronized to remote clients.
+remoteActorCollectionClient :: forall m i a b . (LoggingMonad m, ActorMonad m, NetworkMonad m, Eq i, RemoteActor i b) 
+  => RemActorCollId -- ^ Corresponding server collection id
+  -> Peer -- ^ Server peer
+  -> GameActor m RemActorCollId a [b]
+remoteActorCollectionClient cid peer = makeFixedActor cid $ proc a -> do
+  cs <- peerProcessIndexed peer (ChannelID 0) cid processNet -< emptyClientRemCollState
+  addEvent <- became (not . null) -< clientRemCollNewActors cs
+  remEvent <- became (not . null) -< clientRemCollDelActors cs
+  dynCollection [] -< (a, fmap mkActor <$> addEvent, remEvent)
+  where
+  processNet :: ClientRemCollState i -> RemActorCollNetMessage -> ClientRemCollState i
+  processNet cs msg = case msg of
+    RemActorCollNetSpawn i -> cs { clientRemCollNewActors = fromCounter i : clientRemCollNewActors cs }
+    RemActorCollNetDespawn i -> cs { clientRemCollDelActors = fromCounter i : clientRemCollDelActors cs }
+    RemActorCollRequestOthers -> cs
+
+  mkActor :: i -> GameActor m i a b
+  mkActor _ = undefined
