176c10283ef050035a1640948037860658197f94
NCrashed <NCrashed@gmail.com>
2016-01-13 20:27:54 +0300

    Add documentation for dynamic collection

--- actor/src/Game/GoreAndAsh/Actor/Collection/Data.hs
+++ actor/src/Game/GoreAndAsh/Actor/Collection/Data.hs
@@ -15,14 +15,23 @@ import qualified Data.Foldable as F
 import qualified Data.HashMap.Strict as H 
 import qualified Data.Sequence as S 
 
+-- | Dynamic collection for control wire that automates handling collections of
+-- FRP actors. Ther class defines minimum set of actions that collection should support
+-- to be used as base for collection of actors.
 class (Filterable c, F.Foldable c, Functor c, Traversable c) => DynCollection c where
+  -- | Instance specific constraint for appending function
   type DynConsConstr c o :: Constraint 
   type DynConsConstr c o = ()
 
+  -- | Concat of two collections
   concatDynColl :: c a -> c a -> c a
+  -- | Unzipping of collection
   unzipDynColl :: c (a , b) -> (c a, c b)
+  -- | Ziping collection
   zipDynColl :: c a -> c b -> c (a, b)
+  -- | Getting empty collection
   emptyDynColl :: c a 
+  -- | Adding element to the begining of collection
   consDynColl :: DynConsConstr c a => a -> c a -> c a 
 
 instance DynCollection [] where
@@ -39,9 +48,11 @@ instance DynCollection S.Seq where
   emptyDynColl = S.empty
   consDynColl = (S.<|)
 
+-- | Elements that contains id 
 class (Hashable i, Eq i) => ElementWithId a i where
   elementId :: a -> i 
 
+-- | Order of elements are not preserved
 instance (Eq k, Hashable k) => DynCollection (H.HashMap k) where
   type DynConsConstr (H.HashMap k) o = ElementWithId o k
 
