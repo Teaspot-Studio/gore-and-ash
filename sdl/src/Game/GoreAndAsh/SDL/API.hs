3b15b357e497bf0546e434fdab3a0bb3e1b01a86
NCrashed <NCrashed@gmail.com>
2016-01-14 18:34:59 +0300

    Process SDL events

--- sdl/src/Game/GoreAndAsh/SDL/API.hs
+++ sdl/src/Game/GoreAndAsh/SDL/API.hs
@@ -3,8 +3,12 @@ module Game.GoreAndAsh.SDL.API(
   ) where
 
 import Control.Monad.State.Strict
--- import Control.Wire
+import Data.Sequence (Seq)
 import Prelude hiding (id, (.))
+import qualified Data.Sequence as S 
+
+import SDL hiding (get)
+import SDL.Event 
 
 import Game.GoreAndAsh
 import Game.GoreAndAsh.SDL.State
@@ -12,10 +16,163 @@ import Game.GoreAndAsh.SDL.Module
 
 -- | Low level API for module
 class MonadIO m => SDLMonad m where 
-  sdlStub :: m ()
+
+  -- | Getting window shown events that occurs scince last frame
+  sdlWindowShownEventsM :: m (Seq WindowShownEventData)
+  -- | Getting window hidden events that occurs scince last frame
+  sdlWindowHiddenEventsM :: m (Seq WindowHiddenEventData)
+  -- | Getting window exposed events that occurs scince last frame
+  sdlWindowExposedEventsM :: m (Seq WindowExposedEventData)
+  -- | Getting window move events that occurs scince last frame
+  sdlWindowMovedEventsM :: m (Seq WindowMovedEventData)
+  -- | Getting window resize events that occurs scince last frame
+  -- 
+  -- This is event is always preceded by WindowSizeChangedEvent.
+  sdlWindowResizedEventsM :: m (Seq WindowResizedEventData)
+  -- | Getting window resize events that occurs scince last frame
+  --
+  -- The window size has changed, either as a result of an API call or through the system or user changing the window size; this event is followed by WindowResizedEvent if the size was changed by an external event, i.e. the user or the window manager.
+  sdlWindowSizeChangedEventsM :: m (Seq WindowSizeChangedEventData)
+  -- | Getting window minimization events that occurs scince last frame
+  sdlWindowMinimizedEventsM :: m (Seq WindowMinimizedEventData)
+  -- | Getting window maximization events that occurs scince last frame
+  sdlWindowMaximizedEventsM :: m (Seq WindowMaximizedEventData)
+  -- | Getting window restore events that occurs scince last frame
+  sdlWindowRestoredEventsM :: m (Seq WindowRestoredEventData)
+  -- | Getting window focus events that occurs scince last frame
+  sdlWindowGainedMouseFocusEventsM :: m (Seq WindowGainedMouseFocusEventData)
+  -- | Getting window focus events that occurs scince last frame
+  sdlWindowLostMouseFocusEventsM :: m (Seq WindowLostMouseFocusEventData)
+  -- | Getting window focus events that occurs scince last frame
+  sdlWindowGainedKeyboardFocusEventsM :: m (Seq WindowGainedKeyboardFocusEventData)
+  -- | Getting window focus events that occurs scince last frame
+  sdlWindowLostKeyboardFocusEventsM :: m (Seq WindowLostKeyboardFocusEventData)
+  -- | Getting window close events that occurs scince last frame
+  sdlWindowClosedEventsM :: m (Seq WindowClosedEventData)
+
+  -- | Getting keyboard events that occurs scince last frame
+  sdlKeyboardEventsM :: m (Seq KeyboardEventData)
+  -- | Getting input API events that occurs scince last frame
+  sdlTextEditingEventsM :: m (Seq TextEditingEventData)
+  -- | Getting input API events that occurs scince last frame
+  sdlTextInputEventsM :: m (Seq TextInputEventData)
+
+  -- | Getting mouse events that occurs scince last frame
+  sdlMouseMotionEventsM :: m (Seq MouseMotionEventData)
+  -- | Getting mouse events that occurs scince last frame
+  sdlMouseButtonEventsM :: m (Seq MouseButtonEventData)
+  -- | Getting mouse events that occurs scince last frame
+  sdlMouseWheelEventsM :: m (Seq MouseWheelEventData)
+
+  -- | Getting joystick events that occurs scince last frame
+  sdlJoyAxisEventsM :: m (Seq JoyAxisEventData)
+  -- | Getting joystick events that occurs scince last frame
+  sdlJoyBallEventsM :: m (Seq JoyBallEventData)
+  -- | Getting joystick events that occurs scince last frame
+  sdlJoyHatEventsM :: m (Seq JoyHatEventData)
+  -- | Getting joystick events that occurs scince last frame
+  sdlJoyButtonEventsM :: m (Seq JoyButtonEventData)
+  -- | Getting joystick events that occurs scince last frame
+  sdlJoyDeviceEventsM :: m (Seq JoyDeviceEventData)
+
+  -- | Getting controller events that occurs scince last frame
+  sdlControllerAxisEventsM :: m (Seq ControllerAxisEventData)
+  -- | Getting controller events that occurs scince last frame
+  sdlControllerButtonEventsM :: m (Seq ControllerButtonEventData)
+  -- | Getting controller events that occurs scince last frame
+  sdlControllerDeviceEventsM :: m (Seq ControllerDeviceEventData)
+
+  -- | Getting quit request event
+  sdlQuitEventM :: m Bool
+  -- | Getting user events that occurs scince last frame
+  sdlUserEventsM :: m (Seq UserEventData)
+  -- | Getting video driver specific events that occurs scince last frame
+  sdlSysWMEventsM :: m (Seq SysWMEventData)
+
+  -- | Getting touch events that occurs scince last frame
+  sdlTouchFingerEventsM :: m (Seq TouchFingerEventData)
+  -- | Getting touch events that occurs scince last frame
+  sdlMultiGestureEventsM :: m (Seq MultiGestureEventData)
+  -- | Getting touch events that occurs scince last frame
+  sdlDollarGestureEventsM :: m (Seq DollarGestureEventData)
+  
+  -- | Getting file opened events that occurs scince last frame
+  sdlDropEventsM :: m (Seq DropEventData)
+  -- | Getting clipboard changed events that occurs scince last frame
+  sdlClipboardUpdateEventsM :: m (Seq ClipboardUpdateEventData)
 
 instance {-# OVERLAPPING #-} MonadIO m => SDLMonad (SDLT s m) where
-  sdlStub = return ()
+  sdlWindowShownEventsM = sdlWindowShownEvents <$> get
+  sdlWindowHiddenEventsM = sdlWindowHiddenEvents <$> get
+  sdlWindowExposedEventsM = sdlWindowExposedEvents <$> get
+  sdlWindowMovedEventsM = sdlWindowMovedEvents <$> get
+  sdlWindowResizedEventsM = sdlWindowResizedEvents <$> get
+  sdlWindowSizeChangedEventsM = sdlWindowSizeChangedEvents <$> get
+  sdlWindowMinimizedEventsM = sdlWindowMinimizedEvents <$> get
+  sdlWindowMaximizedEventsM = sdlWindowMaximizedEvents <$> get
+  sdlWindowRestoredEventsM = sdlWindowRestoredEvents <$> get
+  sdlWindowGainedMouseFocusEventsM = sdlWindowGainedMouseFocusEvents <$> get
+  sdlWindowLostMouseFocusEventsM = sdlWindowLostMouseFocusEvents <$> get
+  sdlWindowGainedKeyboardFocusEventsM = sdlWindowGainedKeyboardFocusEvents <$> get
+  sdlWindowLostKeyboardFocusEventsM = sdlWindowLostKeyboardFocusEvents <$> get
+  sdlWindowClosedEventsM = sdlWindowClosedEvents <$> get
+  sdlKeyboardEventsM = sdlKeyboardEvents <$> get
+  sdlTextEditingEventsM = sdlTextEditingEvents <$> get
+  sdlTextInputEventsM = sdlTextInputEvents <$> get
+  sdlMouseMotionEventsM = sdlMouseMotionEvents <$> get
+  sdlMouseButtonEventsM = sdlMouseButtonEvents <$> get
+  sdlMouseWheelEventsM = sdlMouseWheelEvents <$> get
+  sdlJoyAxisEventsM = sdlJoyAxisEvents <$> get
+  sdlJoyBallEventsM = sdlJoyBallEvents <$> get
+  sdlJoyHatEventsM = sdlJoyHatEvents <$> get
+  sdlJoyButtonEventsM = sdlJoyButtonEvents <$> get
+  sdlJoyDeviceEventsM = sdlJoyDeviceEvents <$> get
+  sdlControllerAxisEventsM = sdlControllerAxisEvents <$> get
+  sdlControllerButtonEventsM = sdlControllerButtonEvents <$> get
+  sdlControllerDeviceEventsM = sdlControllerDeviceEvents <$> get
+  sdlQuitEventM = sdlQuitEvent <$> get
+  sdlUserEventsM = sdlUserEvents <$> get
+  sdlSysWMEventsM = sdlSysWMEvents <$> get
+  sdlTouchFingerEventsM = sdlTouchFingerEvents <$> get
+  sdlMultiGestureEventsM = sdlMultiGestureEvents <$> get
+  sdlDollarGestureEventsM = sdlDollarGestureEvents <$> get
+  sdlDropEventsM = sdlDropEvents <$> get
+  sdlClipboardUpdateEventsM = sdlClipboardUpdateEvents <$> get
 
 instance {-# OVERLAPPABLE #-} (MonadIO (mt m), SDLMonad m, MonadTrans mt) => SDLMonad (mt m) where 
-  sdlStub = lift sdlStub
+  sdlWindowShownEventsM = lift sdlWindowShownEventsM
+  sdlWindowHiddenEventsM = lift sdlWindowHiddenEventsM
+  sdlWindowExposedEventsM = lift sdlWindowExposedEventsM
+  sdlWindowMovedEventsM = lift sdlWindowMovedEventsM
+  sdlWindowResizedEventsM = lift sdlWindowResizedEventsM
+  sdlWindowSizeChangedEventsM = lift sdlWindowSizeChangedEventsM
+  sdlWindowMinimizedEventsM = lift sdlWindowMinimizedEventsM
+  sdlWindowMaximizedEventsM = lift sdlWindowMaximizedEventsM
+  sdlWindowRestoredEventsM = lift sdlWindowRestoredEventsM
+  sdlWindowGainedMouseFocusEventsM = lift sdlWindowGainedMouseFocusEventsM
+  sdlWindowLostMouseFocusEventsM = lift sdlWindowLostMouseFocusEventsM
+  sdlWindowGainedKeyboardFocusEventsM = lift sdlWindowGainedKeyboardFocusEventsM
+  sdlWindowLostKeyboardFocusEventsM = lift sdlWindowLostKeyboardFocusEventsM
+  sdlWindowClosedEventsM = lift sdlWindowClosedEventsM
+  sdlKeyboardEventsM = lift sdlKeyboardEventsM
+  sdlTextEditingEventsM = lift sdlTextEditingEventsM
+  sdlTextInputEventsM = lift sdlTextInputEventsM
+  sdlMouseMotionEventsM = lift sdlMouseMotionEventsM
+  sdlMouseButtonEventsM = lift sdlMouseButtonEventsM
+  sdlMouseWheelEventsM = lift sdlMouseWheelEventsM
+  sdlJoyAxisEventsM = lift sdlJoyAxisEventsM
+  sdlJoyBallEventsM = lift sdlJoyBallEventsM
+  sdlJoyHatEventsM = lift sdlJoyHatEventsM
+  sdlJoyButtonEventsM = lift sdlJoyButtonEventsM
+  sdlJoyDeviceEventsM = lift sdlJoyDeviceEventsM
+  sdlControllerAxisEventsM = lift sdlControllerAxisEventsM
+  sdlControllerButtonEventsM = lift sdlControllerButtonEventsM
+  sdlControllerDeviceEventsM = lift sdlControllerDeviceEventsM
+  sdlQuitEventM = lift sdlQuitEventM
+  sdlUserEventsM = lift sdlUserEventsM
+  sdlSysWMEventsM = lift sdlSysWMEventsM
+  sdlTouchFingerEventsM = lift sdlTouchFingerEventsM
+  sdlMultiGestureEventsM = lift sdlMultiGestureEventsM
+  sdlDollarGestureEventsM = lift sdlDollarGestureEventsM
+  sdlDropEventsM = lift sdlDropEventsM
+  sdlClipboardUpdateEventsM = lift sdlClipboardUpdateEventsM
--- sdl/src/Game/GoreAndAsh/SDL/Module.hs
+++ sdl/src/Game/GoreAndAsh/SDL/Module.hs
@@ -5,7 +5,13 @@ module Game.GoreAndAsh.SDL.Module(
 
 import Control.Monad.Catch
 import Control.Monad.Fix 
+import Control.Monad.IO.Class 
 import Control.Monad.State.Strict
+import qualified Data.Foldable as F 
+import qualified Data.Sequence as S 
+
+import SDL
+import SDL.Event 
 
 import Game.GoreAndAsh
 import Game.GoreAndAsh.SDL.State
@@ -16,11 +22,57 @@ newtype SDLT s m a = SDLT { runSDLT :: StateT (SDLState s) m a }
 instance GameModule m s => GameModule (SDLT s m) (SDLState s) where 
   type ModuleState (SDLT s m) = SDLState s
   runModule (SDLT m) s = do
-    ((a, s'), nextState) <- runModule (runStateT m s) (sdlNextState s)
-    return (a, flashSDLState $ s' { 
+    s' <- processEvents s 
+    ((a, s''), nextState) <- runModule (runStateT m s') (sdlNextState s')
+    return (a, flashSDLState $ s'' { 
         sdlNextState = nextState
       })
 
   newModuleState = emptySDLState <$> newModuleState
-  withModule _ = id
-  cleanupModule _ = return ()
+  withModule _ io = initializeAll >> io
+  cleanupModule _ = quit
+
+-- | Catch all SDL events
+processEvents :: MonadIO m => SDLState s -> m (SDLState s)
+processEvents s = do 
+  es <- pollEvents
+  return $! F.foldl' process s (eventPayload <$> es)
+  where 
+  process s e = case e of 
+    WindowShownEvent d -> s { sdlWindowShownEvents = sdlWindowShownEvents s S.|> d }
+    WindowHiddenEvent d -> s { sdlWindowHiddenEvents = sdlWindowHiddenEvents s S.|> d }
+    WindowExposedEvent d -> s { sdlWindowExposedEvents = sdlWindowExposedEvents s S.|> d }
+    WindowMovedEvent d -> s { sdlWindowMovedEvents = sdlWindowMovedEvents s S.|> d }
+    WindowResizedEvent d -> s { sdlWindowResizedEvents = sdlWindowResizedEvents s S.|> d }
+    WindowSizeChangedEvent d -> s { sdlWindowSizeChangedEvents = sdlWindowSizeChangedEvents s S.|> d }
+    WindowMinimizedEvent d -> s { sdlWindowMinimizedEvents = sdlWindowMinimizedEvents s S.|> d }
+    WindowMaximizedEvent d -> s { sdlWindowMaximizedEvents = sdlWindowMaximizedEvents s S.|> d }
+    WindowRestoredEvent d -> s { sdlWindowRestoredEvents = sdlWindowRestoredEvents s S.|> d }
+    WindowGainedMouseFocusEvent d -> s { sdlWindowGainedMouseFocusEvents = sdlWindowGainedMouseFocusEvents s S.|> d }
+    WindowLostMouseFocusEvent d -> s { sdlWindowLostMouseFocusEvents = sdlWindowLostMouseFocusEvents s S.|> d }
+    WindowGainedKeyboardFocusEvent d -> s { sdlWindowGainedKeyboardFocusEvents = sdlWindowGainedKeyboardFocusEvents s S.|> d }
+    WindowLostKeyboardFocusEvent d -> s { sdlWindowLostKeyboardFocusEvents = sdlWindowLostKeyboardFocusEvents s S.|> d }
+    WindowClosedEvent d -> s { sdlWindowClosedEvents = sdlWindowClosedEvents s S.|> d }
+    KeyboardEvent d -> s { sdlKeyboardEvents = sdlKeyboardEvents s S.|> d }
+    TextEditingEvent d -> s { sdlTextEditingEvents = sdlTextEditingEvents s S.|> d }
+    TextInputEvent d -> s { sdlTextInputEvents = sdlTextInputEvents s S.|> d }
+    MouseMotionEvent d -> s { sdlMouseMotionEvents = sdlMouseMotionEvents s S.|> d }
+    MouseButtonEvent d -> s { sdlMouseButtonEvents = sdlMouseButtonEvents s S.|> d }
+    MouseWheelEvent d -> s { sdlMouseWheelEvents = sdlMouseWheelEvents s S.|> d }
+    JoyAxisEvent d -> s { sdlJoyAxisEvents = sdlJoyAxisEvents s S.|> d }
+    JoyBallEvent d -> s { sdlJoyBallEvents = sdlJoyBallEvents s S.|> d }
+    JoyHatEvent d -> s { sdlJoyHatEvents = sdlJoyHatEvents s S.|> d }
+    JoyButtonEvent d -> s { sdlJoyButtonEvents = sdlJoyButtonEvents s S.|> d }
+    JoyDeviceEvent d -> s { sdlJoyDeviceEvents = sdlJoyDeviceEvents s S.|> d }
+    ControllerAxisEvent d -> s { sdlControllerAxisEvents = sdlControllerAxisEvents s S.|> d }
+    ControllerButtonEvent d -> s { sdlControllerButtonEvents = sdlControllerButtonEvents s S.|> d }
+    ControllerDeviceEvent d -> s { sdlControllerDeviceEvents = sdlControllerDeviceEvents s S.|> d }
+    QuitEvent -> s { sdlQuitEvent = True }
+    UserEvent d -> s { sdlUserEvents = sdlUserEvents s S.|> d }
+    SysWMEvent d -> s { sdlSysWMEvents = sdlSysWMEvents s S.|> d }
+    TouchFingerEvent d -> s { sdlTouchFingerEvents = sdlTouchFingerEvents s S.|> d }
+    MultiGestureEvent d -> s { sdlMultiGestureEvents = sdlMultiGestureEvents s S.|> d }
+    DollarGestureEvent d -> s { sdlDollarGestureEvents = sdlDollarGestureEvents s S.|> d }
+    DropEvent d -> s { sdlDropEvents = sdlDropEvents s S.|> d }
+    ClipboardUpdateEvent d -> s { sdlClipboardUpdateEvents = sdlClipboardUpdateEvents s S.|> d }
+    _ -> s
