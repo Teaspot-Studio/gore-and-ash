2473f16b6431f4f8843d8a6890d738c8e023f08d
NCrashed <NCrashed@gmail.com>
2016-01-14 17:21:38 +0300

    SDL simple app

--- testapp/src/client/Main.hs
+++ testapp/src/client/Main.hs
@@ -14,20 +14,9 @@ module Main where
 import System.Environment
 import Text.Read 
 
-import Data.Vec ((:.)(..), Vec3, Vec4)
-
-import qualified Data.Vector.Storable as V
-
-import qualified Graphics.Rendering.OpenGL.GL as GL
-
-import Andromeda.Simple.Expr
-import Andromeda.Simple.Type
-import Andromeda.Simple.StdLib
-import Andromeda.Simple.GLSL
-
-import Andromeda.Simple.Render.Mesh
-import Andromeda.Simple.Render.VertexBuffer
-import Andromeda.Simple.Render.Compile
+import SDL
+import Linear (V4(..))
+import Control.Monad (unless)
 
 gameFPS :: Int 
 gameFPS = 60 
@@ -75,43 +64,22 @@ main = withModule (Proxy :: Proxy AppMonad) $ do
 
 main :: IO ()
 main = do
-    win <- openWindow
-    initGL win
+  initializeAll
+  window <- createWindow "My SDL Application" defaultWindow
+  renderer <- createRenderer window (-1) defaultRenderer
+  appLoop renderer
 
-    putStrLn $ toGLSL vertShader
-    putStrLn $ toGLSL fragShader
-    prog <- addMesh myMesh =<< compile vertShader fragShader
-
-    mainLoop win prog
-
-glPosition :: Expr (Vec4 Float)
-glPosition = fetch "vertex" (Vec3T SFloat) +: 1
-
-vertShader :: Statement ()
-vertShader = do
-    "gl_Position" =: glPosition
-    out "fragPos" glPosition
-
-outColor :: Expr (Vec3 Float)
-outColor =
-    let fragPos = fetch   "fragPos" (Vec4T SFloat)
-        inColor = uniform "inColor" (Vec3T SFloat)
-    in vzipWith avg inColor fragPos
-  where
-    avg x y = (x + y) / 2
-
-fragShader :: Statement ()
-fragShader = out "color" outColor
-
-myMesh :: Mesh
-myMesh = Mesh
-    [("vertex",  MeshAttribute $ V.fromList triangle),
-     ("inColor", MeshUniform   $ Uniform color)] Triangles
-
-triangle :: [Vec3 Float]
-triangle = [(-1):.(-1):.0:.(),
-              1 :.(-1):.0:.(),
-              0 :.  1 :.0:.()]
-
-color :: GL.Vertex3 GL.GLfloat
-color = GL.Vertex3 0.5 0.5 0.5
+appLoop :: Renderer -> IO ()
+appLoop renderer = do
+  events <- pollEvents
+  let eventIsQPress event =
+        case eventPayload event of
+          KeyboardEvent keyboardEvent ->
+            keyboardEventKeyMotion keyboardEvent == Pressed &&
+            keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
+          _ -> False
+      qPressed = not (null (filter eventIsQPress events))
+  rendererDrawColor renderer $= V4 0 0 255 255
+  clear renderer
+  present renderer
+  unless qPressed (appLoop renderer)
