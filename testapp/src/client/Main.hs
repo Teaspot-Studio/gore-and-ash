22d3dd8911a0f0d61fdece36229f072ba2abeaa5
NCrashed <NCrashed@gmail.com>
2016-01-14 16:58:17 +0300

    Andromeda test

--- testapp/src/client/Main.hs
+++ testapp/src/client/Main.hs
@@ -13,7 +13,21 @@ module Main where
 --import Render 
 import System.Environment
 import Text.Read 
-import Graphics.Gloss.Interface.IO.Game
+
+import Data.Vec ((:.)(..), Vec3, Vec4)
+
+import qualified Data.Vector.Storable as V
+
+import qualified Graphics.Rendering.OpenGL.GL as GL
+
+import Andromeda.Simple.Expr
+import Andromeda.Simple.Type
+import Andromeda.Simple.StdLib
+import Andromeda.Simple.GLSL
+
+import Andromeda.Simple.Render.Mesh
+import Andromeda.Simple.Render.VertexBuffer
+import Andromeda.Simple.Render.Compile
 
 gameFPS :: Int 
 gameFPS = 60 
@@ -61,10 +75,43 @@ main = withModule (Proxy :: Proxy AppMonad) $ do
 
 main :: IO ()
 main = do
-  -- gs <- newGameState mainWire
-  playIO display 0 60 () renderGame eventUpdate gameUpdate
+    win <- openWindow
+    initGL win
+
+    putStrLn $ toGLSL vertShader
+    putStrLn $ toGLSL fragShader
+    prog <- addMesh myMesh =<< compile vertShader fragShader
+
+    mainLoop win prog
+
+glPosition :: Expr (Vec4 Float)
+glPosition = fetch "vertex" (Vec3T SFloat) +: 1
+
+vertShader :: Statement ()
+vertShader = do
+    "gl_Position" =: glPosition
+    out "fragPos" glPosition
+
+outColor :: Expr (Vec3 Float)
+outColor =
+    let fragPos = fetch   "fragPos" (Vec4T SFloat)
+        inColor = uniform "inColor" (Vec3T SFloat)
+    in vzipWith avg inColor fragPos
   where
-    display = InWindow "Client" (0, 0) (800, 600)
-    renderGame _ = pure $ Circle 80
-    eventUpdate _ = pure 
-    gameUpdate _ gs = return gs
+    avg x y = (x + y) / 2
+
+fragShader :: Statement ()
+fragShader = out "color" outColor
+
+myMesh :: Mesh
+myMesh = Mesh
+    [("vertex",  MeshAttribute $ V.fromList triangle),
+     ("inColor", MeshUniform   $ Uniform color)] Triangles
+
+triangle :: [Vec3 Float]
+triangle = [(-1):.(-1):.0:.(),
+              1 :.(-1):.0:.(),
+              0 :.  1 :.0:.()]
+
+color :: GL.Vertex3 GL.GLfloat
+color = GL.Vertex3 0.5 0.5 0.5
