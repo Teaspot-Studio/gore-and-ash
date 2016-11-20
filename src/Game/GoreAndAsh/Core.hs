5d395ebfd9a3bf52a0c601a0623e872f50770de9
Anton Gushcha <ncrashed@gmail.com>
2016-11-20 23:05:23 +0300

    Move old implementation to separate folder

--- src/Game/GoreAndAsh/Core.hs
+++ src/Game/GoreAndAsh/Core.hs
@@ -8,60 +8,10 @@ Maintainer  : ncrashed@gmail.com
 Stability   : experimental
 Portability : POSIX
 
-The core of all engine. It contains generic arrow operations and helpers,
-definition of core module system, game session declaration and utilities
-to control main loop of application.
+The core of all engine. It contains generic reactive operations and helpers,
+definition of core module system and utilities to control main loop of
+application.
 -}
 module Game.GoreAndAsh.Core(
-  -- * Reexports of used time types
-    GameTime
-  , GameSession
-  , NominalDiffTime
-  -- * Game loop control
-  , GameState
-  , stepGame
-  , newGameState
-  , newGameStateM
-  , cleanupGameState
-  -- * Core module definition
-  , GameMonadT
-  , GameModule(..)
-  , ModuleStack
-  -- * Arrow combinators and helpers
-  , GameWire
-  -- ** Lifting monad to arrow
-  , liftGameMonad
-  , liftGameMonad1
-  , liftGameMonad2
-  , liftGameMonad3
-  , liftGameMonad4
-  , liftGameMonadOnce
-  , liftGameMonad1Once
-  , liftGameMonad2Once
-  , liftGameMonad3Once
-  , liftGameMonad4Once
-  -- ** Event functions
-  , once'
-  , mapE
-  , filterE
-  , filterEG
-  , filterEGM
-  , filterJustE
-  , filterJustLE
-  , liftGameMonadEvent1
-  , changes
-  -- ** Helpers
-  , stateWire
-  , chainWires
-  , dispense
-  , dDispense
-  , withInit
-  , nothingInhibit
-  -- ** Time utilities
-  , deltaTime
-  ) where
 
-import Game.GoreAndAsh.Core.Arrow as X
-import Game.GoreAndAsh.Core.Monad as X
-import Game.GoreAndAsh.Core.Session as X
-import Game.GoreAndAsh.Core.State as X
+  ) where
--- src_old/Game/GoreAndAsh/Math.hs
+++ src_old/Game/GoreAndAsh/Math.hs
@@ -28,7 +28,7 @@ module Game.GoreAndAsh.Math(
 import Linear
 
 -- | Scale matrix for 3D transformation
-scale :: Num a => V3 a -> M44 a
+scale :: Num a => V3 a -> M44 a 
 scale (V3 x y z) = V4
   (V4 x 0 0 0)
   (V4 0 y 0 0)
@@ -36,60 +36,60 @@ scale (V3 x y z) = V4
   (V4 0 0 0 1)
 
 -- | Rotation around Z axis for 3D transformation
-rotationZ :: Floating a => a -> M44 a
-rotationZ a = V4
+rotationZ :: Floating a => a -> M44 a 
+rotationZ a = V4 
   (V4 (cos a) (- sin a) 0 0)
   (V4 (sin a) (  cos a) 0 0)
   (V4 0 0 1 0)
   (V4 0 0 0 1)
 
 -- | Translation matrix for 3D transformation
-translate :: Num a => V3 a -> M44 a
-translate (V3 x y z) = V4
+translate :: Num a => V3 a -> M44 a 
+translate (V3 x y z) = V4 
   (V4 1 0 0 x)
   (V4 0 1 0 y)
   (V4 0 0 1 z)
   (V4 0 0 0 1)
 
 -- | Scale matrix for 2D transformation
-scale2D :: Num a => V2 a -> M33 a
+scale2D :: Num a => V2 a -> M33 a 
 scale2D (V2 x y) = V3
   (V3 x 0 0)
   (V3 0 y 0)
   (V3 0 0 1)
 
 -- | Rotation matrix for 2D transformation
-rotation2D :: Floating a => a -> M33 a
-rotation2D a = V3
+rotation2D :: Floating a => a -> M33 a 
+rotation2D a = V3 
   (V3 (cos a) (- sin a) 0)
   (V3 (sin a) (  cos a) 0)
   (V3 0 0 1)
 
 -- | Translation matrix for 2D transformation
-translate2D :: Num a => V2 a -> M33 a
-translate2D (V2 x y) = V3
+translate2D :: Num a => V2 a -> M33 a 
+translate2D (V2 x y) = V3 
   (V3 1 0 x)
   (V3 0 1 y)
   (V3 0 0 1)
 
 -- | Transform to homogenius coordinates
-toHom2D :: Num a => V2 a -> V3 a
+toHom2D :: Num a => V2 a -> V3 a 
 toHom2D (V2 x y) = V3 x y 1
 
 -- | Transform from homogenius coordinates
-fromHom2D :: Floating a => V3 a -> V2 a
+fromHom2D :: Floating a => V3 a -> V2 a 
 fromHom2D (V3 x y w) = V2 (x/w) (y/w)
 
 -- | Applies transformation matrix to vector
-applyTransform2D :: Floating a => M33 a -> V2 a -> V2 a
+applyTransform2D :: Floating a => M33 a -> V2 a -> V2 a 
 applyTransform2D mt v = fromHom2D $ mt !* toHom2D v
 
 -- | Viewport transformation matrix
-viewportTransform2D :: Floating a
+viewportTransform2D :: Floating a 
   => V2 a -- ^ Viewport left top corner
   -> V2 a -- ^ Viewport right bottom corner
   -> M33 a
-viewportTransform2D (V2 l t) (V2 r b) = V3
+viewportTransform2D (V2 l t) (V2 r b) = V3 
   (V3 ((r-l)/2) 0          ((r+l)/2))
   (V3 0         (-(t-b)/2) ((t+b)/2))
   (V3 0         0          1)
